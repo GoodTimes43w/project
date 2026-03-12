932920bd4bde79ba400b5736ae959cd12f5cda99
krixerskib223 <gd2500@panthernet.net>
2026-03-12 15:17:39 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+maqueenPlusV2.I2CInit()
+maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 25)
+basic.pause(3000)
+maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
