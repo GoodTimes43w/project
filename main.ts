a73439037c3dcc35197aa3a7f354b77e7e97a4c6
krixerskib223 <gd2500@panthernet.net>
2026-03-12 15:31:51 -0400

    Speed Test

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 maqueenPlusV2.I2CInit()
-maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 25)
+maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 300)
 basic.pause(3000)
 maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
