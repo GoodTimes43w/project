586e9ccdce00a4c70eb1fd5fbcda0f9910ae7e1d
krixerskib223 <gd2500@panthernet.net>
2026-03-17 08:24:31 -0400

    Restore '3/15/2026, 1:12:24 PM puishedsadjasi'

--- main.ts
+++ main.ts
@@ -4,11 +4,14 @@ let Block_Pos_x = 0
 let Block_Pos_y = 0
 maqueenPlusV2.I2CInit()
 maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-matrixLidarDistance.initialize(matrixLidarDistance.Addr.Addr4, matrixLidarDistance.Matrix.OBS)
-matrixLidarDistance.setObstacleDistance(200)
-matrixLidarDistance.getData()
+basic.pause(2500)
+maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
+basic.pause(1000)
+let StartDetection = true
+maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 25)
+maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Backward, 25)
+basic.pause(3800)
 basic.forever(function () {
-    let StartDetection = 0
     if (StartDetection) {
         huskylens.initI2c()
         huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
@@ -21,32 +24,8 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (matrixLidarDistance.obstacleSuggestion() == 1) {
-        maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
-    } else {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-    }
-})
-basic.forever(function () {
     CurrentPos00 = 0
 })
 basic.forever(function () {
     Turn_angle = CurrentPos00 - Block_Pos_x
-})
-basic.forever(function () {
-    if (Turn_angle < 0) {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Backward, 100)
-    }
-})
-basic.forever(function () {
-    if (Turn_angle > 0) {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Backward, 100)
-    }
-})
-basic.forever(function () {
-    if (Turn_angle == 0) {
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 100)
-    }
 })
